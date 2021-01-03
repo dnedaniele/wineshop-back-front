f3bd1e87440f361a3b16e988a43c5651e3fa8e0d
Daniele Esposito <esp.dne@gmail.com>
2021-01-03 21:52:19 +0100

    ready to start

--- back/app.js
+++ back/app.js
@@ -1,7 +1,23 @@
 const express = require('express');
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const cors = require("cors"); 
+require("dotenv").config();
 
 const app = express(); 
+
+// MIDDLEWARES
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//Connect to MongoDB
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true  
+})
+.then(()=> console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 // ROUTES
 
@@ -9,4 +25,8 @@ app.get('/',(req, res)=>{
   res.send("this is the homepage")
 });
 
-app.listen(3000)
+// Listen
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> console.log(`Server run at port ${PORT}`));
